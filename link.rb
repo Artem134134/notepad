f3582f9da8ec8c523c52299034621932a500079a
Artem134134 <car322132@gmail.com>
2023-03-02 00:07:00 +0300

    The Link class implements the method read from console

--- link.rb
+++ link.rb
@@ -7,7 +7,11 @@ class Link < Post
   end
 
   def read_from_console
+    puts "Link address: "
+    @url = STDIN.gets.chomp 
 
+    puts "Link description ?"
+    @text = STDIN.gets.chomp
   end
 
   def to_strings
